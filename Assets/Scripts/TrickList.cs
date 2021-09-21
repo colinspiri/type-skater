0b9c0ced96d3a49c87aecb99f46c4cd982f45ed3
Colin Spiridonov <spiridon@usc.edu>
2021-09-21 11:23:45 -0700

    Display only available tricks

--- Assets/Scripts/TrickList.cs
+++ Assets/Scripts/TrickList.cs
@@ -11,8 +11,11 @@ public class TrickList : MonoBehaviour
     public TextMeshProUGUI trickDisplay;
 
     private void Update() {
-        trickDisplay.text = "Tricks\n";
+        trickDisplay.text = "Available Tricks\n";
         foreach (Word w in typingManager.words) {
+            // skip tricks that you've already done, but not grind
+            if (typingManager.currentTricks.Contains(w) && w.text != "grind") continue;
+            // skip if not in state
             if (!w.availableInStates.Contains(Player.Instance.state)) continue;
             trickDisplay.text += w.trickScore + " " + '\t' + w.text + '\n';
         }
