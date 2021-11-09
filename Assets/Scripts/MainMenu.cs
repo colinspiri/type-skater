23f825f4d1ae7ae4cde80b89c94964ba1f901df6
Colin Spiridonov <spiridon@usc.edu>
2021-11-08 21:24:29 -0800

    Fixed main menu and typing menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelect;
+    public GameObject options;
+    public GameObject credits;
+
+    public enum MenuState {
+        MainMenu,
+        LevelSelect,
+        Options,
+        Credits
+    }
+    private MenuState currentMenu;
+
+    private void Start() {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(false);
+        options.SetActive(false);
+        credits.SetActive(false);
+        ChangeMenu(MenuState.MainMenu);
+    }
+
+    private void Update() {
+        // escape to go back to main menu
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            BackToMainMenu();
+        }
+    }
+
+    private void ChangeMenu(MenuState newMenu) {
+        // disable current menu
+        if(currentMenu == MenuState.MainMenu) mainMenu.SetActive(false);
+        else if(currentMenu == MenuState.LevelSelect) levelSelect.SetActive(false);
+        else if (currentMenu == MenuState.Options) options.SetActive(false);
+        else if(currentMenu == MenuState.Credits) credits.SetActive(false);
+        
+        // enable new menu
+        currentMenu = newMenu;
+        if(currentMenu == MenuState.MainMenu) mainMenu.SetActive(true);
+        else if(currentMenu == MenuState.LevelSelect) levelSelect.SetActive(true);
+        else if (currentMenu == MenuState.Options) options.SetActive(true);
+        else if(currentMenu == MenuState.Credits) credits.SetActive(true);
+    }
+
+    public void Play() {
+        // Debug.Log("play");
+        ChangeMenu(MenuState.LevelSelect);
+    }
+
+    public void Options() {
+        // Debug.Log("options");
+        ChangeMenu(MenuState.Options);
+    }
+    public void Credits() {
+        // Debug.Log("credits");
+        ChangeMenu(MenuState.Credits);
+    }
+    public void Quit() {
+        // Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void City() {
+        // Debug.Log("city");
+        // load city scene
+        SceneManager.LoadScene("Level1");
+    }
+    public void Park() {
+        Debug.Log("park");
+        // load park scene
+    }
+    public void Infinite() {
+        // Debug.Log("infinite");
+        // load infinite mode
+        SceneManager.LoadScene("Infinite");
+    }
+    public void BackToMainMenu() {
+        // Debug.Log("back to main menu");
+        ChangeMenu(MenuState.MainMenu);
+    }
+}
--- Assets/Scripts/TypingMenu.cs
+++ Assets/Scripts/TypingMenu.cs
@@ -5,121 +5,114 @@ using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TypingMenu : MonoBehaviour {
-    public GameObject mainMenu;
-    public GameObject levelSelect;
-    public GameObject credits;
-    public GameObject options;
-
+    
     public List<MenuOption> menuOptions;
-    
-    public enum MenuState {
-        MainMenu,
-        LevelSelect,
-        Options,
-        Credits
-    }
-    private MenuState currentMenu;
-    
+
+    private int currentOption = -1;
+
+    private KeyCode upKey = KeyCode.UpArrow;
+    private KeyCode downKey = KeyCode.DownArrow;
+    // private KeyCode rightKey = KeyCode.RightArrow;
+    // private KeyCode leftKey = KeyCode.LeftArrow;
+    private readonly List<KeyCode> selectKeys = new List<KeyCode> {KeyCode.Return, KeyCode.RightArrow, KeyCode.Space};
+
     // Start is called before the first frame update
     void Start() {
-        mainMenu.SetActive(false);
-        levelSelect.SetActive(false);
-        options.SetActive(false);
-        credits.SetActive(false);
-        ChangeMenu(MenuState.MainMenu);
         foreach (MenuOption menuOption in menuOptions) {
-            menuOption.GetText();
+            menuOption.Initialize();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // escape to go back to main menu
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            BackToMainMenu();
+        // arrow keys to navigate between options
+        if (Input.GetKeyDown(downKey)) {
+            int newCurrentOption = currentOption + 1;
+            newCurrentOption %= menuOptions.Count;
+            SetCurrentOption(newCurrentOption);
+            HighlightCurrentOption();
+            return;
         }
-        
+        if (Input.GetKeyDown(upKey)) {
+            int newCurrentOption = currentOption - 1;
+            if (newCurrentOption < 0) newCurrentOption = menuOptions.Count - 1;
+            SetCurrentOption(newCurrentOption);
+            HighlightCurrentOption();
+            return;
+        }
+        // if (menuOptions[currentOption].slider != null) {
+        //     if (Input.GetKeyDown(rightKey)) {
+        //         // increase slider value
+        //         return;
+        //     }
+        //     if (Input.GetKeyDown(leftKey)) {
+        //         // decrease slider value
+        //         return;
+        //     }
+        // }
+        // select current option
+        if (currentOption != -1) {
+            foreach (KeyCode selectKey in selectKeys) {
+                if (Input.GetKeyDown(selectKey)) {
+                    menuOptions[currentOption].onSelect?.Invoke();
+                    return;
+                }
+            }
+        }
+        // type to select
         string input = Input.inputString;
         if (input.Equals("")) return;
         
         char c = input[0];
-        MenuOption currentOption = null;
-        foreach (MenuOption option in menuOptions) {
-            if (option.menuState != currentMenu) continue;
+        int typedOption = -1;
+        for (int i = 0; i < menuOptions.Count; i++) {
+            MenuOption option = menuOptions[i];
             // if the current input matches a word
             if (option.ContinueText(c)) {
-                if (currentOption == null) {
-                    currentOption = option;
+                if (typedOption == -1) {
+                    Debug.Log(option.text + " chosen as typedOption");
+                    typedOption = i;
                 }
-                else if(option.GetTyped().Length > currentOption.GetTyped().Length) {
-                    currentOption.Clear();
-                    currentOption = option;
+                else if(option.GetTyped().Length > menuOptions[typedOption].GetTyped().Length) {
+                    Debug.Log(option.text + " has gettyped " + option.GetTyped() + " vs " + menuOptions[typedOption] + " has gettyped " + menuOptions[typedOption].GetTyped());
+                    menuOptions[typedOption].Clear();
+                    typedOption = i;
                 }
             }
         }
-    }
-    
-    public void Play() {
-        // Debug.Log("play");
-        ChangeMenu(MenuState.LevelSelect);
+        Debug.Log(typedOption);
+        SetCurrentOption(typedOption);
     }
 
-    public void Options() {
-        // Debug.Log("options");
-        ChangeMenu(MenuState.Options);
+    private void SetCurrentOption(int newCurrent) {
+        currentOption = newCurrent;
+        // clear all other options
+        for(int i = 0; i < menuOptions.Count; i++) {
+            if (i == currentOption) continue;
+            menuOptions[i].Clear();
+        }
     }
-    public void Credits() {
-        // Debug.Log("credits");
-        ChangeMenu(MenuState.Credits);
-    }
-    public void Quit() {
-        // Debug.Log("quit");
-        Application.Quit();
-    }
-
-    public void City() {
-        // Debug.Log("city");
-        // load city scene
-        SceneManager.LoadScene("Level1");
-    }
-    public void Park() {
-        Debug.Log("park");
-        // load park scene
-    }
-    public void Infinite() {
-        // Debug.Log("infinite");
-        // load infinite mode
-        SceneManager.LoadScene("Infinite");
-    }
-    public void BackToMainMenu() {
-        // Debug.Log("back to main menu");
-        ChangeMenu(MenuState.MainMenu);
-    }
-
-    private void ChangeMenu(MenuState newMenu) {
-        // disable current menu
-        if(currentMenu == MenuState.MainMenu) mainMenu.SetActive(false);
-        else if(currentMenu == MenuState.LevelSelect) levelSelect.SetActive(false);
-        else if (currentMenu == MenuState.Options) options.SetActive(false);
-        else if(currentMenu == MenuState.Credits) credits.SetActive(false);
-        
-        // enable new menu
-        currentMenu = newMenu;
-        if(currentMenu == MenuState.MainMenu) mainMenu.SetActive(true);
-        else if(currentMenu == MenuState.LevelSelect) levelSelect.SetActive(true);
-        else if (currentMenu == MenuState.Options) options.SetActive(true);
-        else if(currentMenu == MenuState.Credits) credits.SetActive(true);
+    private void HighlightCurrentOption() {
+        // clear all options
+        foreach (var menuOption in menuOptions) {
+            menuOption.Clear();
+        }
+        // highlight current
+        if (currentOption != -1) {
+            menuOptions[currentOption].Highlight();
+        }
     }
 }
 
 [Serializable]
 public class MenuOption {
     public TextMeshProUGUI menuText;
-    public TypingMenu.MenuState menuState;
+    public Slider slider;
+    
     public UnityEvent onSelect;
     
     private TextMeshProUGUI highlightedText;
@@ -128,9 +121,10 @@ public class MenuOption {
     private string hasTyped;
     private int curChar;
 
-    public void GetText() {
+    public void Initialize() {
         text = menuText.text;
         highlightedText = menuText.gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        onSelect.AddListener(Clear);
     }
     
     public bool ContinueText(char c)
@@ -153,6 +147,10 @@ public class MenuOption {
         // if c doesn't match
         Clear();
         return false;
+    }
+
+    public void Highlight() {
+        highlightedText.text = text;
     }
     
     public void Clear()
