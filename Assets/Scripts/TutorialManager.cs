67f3abba8b62f92f3173c52feddf1ca1d9c87355
Colin Spiridonov <spiridon@usc.edu>
2021-11-11 00:58:00 -0800

    Finished level0 tutorial

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,42 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour {
+    private float previousTimeScale;
+    public Color greyedOutColor;
+    private Color originalColor;
+    
+    // push text
+    public TextMeshProUGUI pushText;
+    
+    // ollie text
+    public TextMeshProUGUI ollieText;
+
     // trick tutorial
     private bool trickTutorialDone;
     public float trickTutorialDelay;
     public TrickList trickList;
-    public GameObject trickCanvas;
-    private float previousTimeScale;
+    public GameObject trickCanvasPrefab;
+    private TextMeshProUGUI trickText;
     
     // safe tutorial
+    private bool safeTutorialStarted;
     private bool safeTutorialDone;
     public float safeTutorialDelay;
-    
+    public GameObject safeCanvasPrefab;
+    private TextMeshProUGUI safeText;
+
     // Start is called before the first frame update
     void Start() {
         Player.Instance.onJump += () => {
             if(!trickTutorialDone) StartTrickTutorial();
         };
+        Player.Instance.onSafeLanding += () => {
+            safeTutorialDone = true;
+        };
+        Player.Instance.onUnsafeLanding += () => {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        };
         TypingManager.Instance.onCompleteWord += word => {
-            if (word.Equals("fakie") && !trickTutorialDone) {
+            if (word.Equals("push")) {
+                pushText.color = greyedOutColor;
+            }
+            else if (word.Equals("ollie")) {
+                ollieText.color = greyedOutColor;
+            }
+            else if (word.Equals("fakie") && !trickTutorialDone) {
                 Time.timeScale = previousTimeScale;
+                trickText.color = greyedOutColor;
                 trickTutorialDone = true;
                 // start safe tutorial
                 if(!safeTutorialDone) StartSafeTutorial();
             }
         };
+        originalColor = pushText.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (trickTutorialDone && safeTutorialStarted && !safeTutorialDone) {
+            if (Input.GetKey(KeyCode.Return)) {
+                Time.timeScale = previousTimeScale;
+                safeText.color = greyedOutColor;
+            }
+            else safeText.color = originalColor;
+        }
     }
 
     private void StartSafeTutorial() {
-        safeTutorialDone = true;
+        StartCoroutine(SafeTutorial());
     }
 
     IEnumerator SafeTutorial() {
@@ -45,6 +80,12 @@ public class TutorialManager : MonoBehaviour {
         yield return new WaitForSeconds(seconds);
         
         // start safe tutorial
+        previousTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+        safeTutorialStarted = true;
+        // spawn safe canvas that prompts player to hold ENTER
+        var safeCanvas = Instantiate(safeCanvasPrefab, Player.Instance.transform.position, Quaternion.identity);
+        safeText = safeCanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
     }
 
     private void StartTrickTutorial() {
@@ -59,7 +100,8 @@ public class TutorialManager : MonoBehaviour {
         previousTimeScale = Time.timeScale;
         Time.timeScale = 0;
         // spawn trick canvas that prompts player to type trick
-        Instantiate(trickCanvas, Player.Instance.transform.position, Quaternion.identity);
+        var trickCanvas = Instantiate(trickCanvasPrefab, Player.Instance.transform.position, Quaternion.identity);
+        trickText = trickCanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         // show trick list
         trickList.gameObject.SetActive(true);
     }
